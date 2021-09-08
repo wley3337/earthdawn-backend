577544ab51784ec23deb8652d5da998417147ebc
Will Ley <wley3337@gmail.com>
2021-09-07 20:28:50 -0400

    Update login to pull from db

--- api/routes/login.py
+++ api/routes/login.py
@@ -2,6 +2,9 @@ from flask import Blueprint, jsonify, request
 from http import HTTPStatus
 from flasgger import swag_from
 
+from api.models.user import User
+from api.utils.password_helpers import create_password_digest
+
 login = Blueprint('login', __name__)
 
 
@@ -16,14 +19,9 @@ def login_user():
     """Logs a user into application and adds a JWT HTTP only cookie"""
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    return jsonify({
-        "success": True,
-        "username": username,
-        "password": password
-    })
+    user = User.find_by_username(username)
 
-
-# if username != "test" or password != 'test':
-#     return jsonify({"msg": "Bad username or password"}), 401
-# access_token = create_access_token(identity=username)
-# return jsonify(access_token=access_token)
+    if user and create_password_digest(password) == user.password_digest:
+        return jsonify({"username": username, "password": password}), 200
+    else:
+        return jsonify({"message": "Username or Password is incorrect."}), 404
