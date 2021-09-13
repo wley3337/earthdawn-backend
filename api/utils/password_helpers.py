d2b8555daed5752461c735efcd00bad7f7fc65f3
wley3337 <wley3337@gmail.com>
2021-09-13 18:43:36 -0400

    Update password helpers

--- api/utils/password_helpers.py
+++ api/utils/password_helpers.py
@@ -7,5 +7,20 @@ def create_password_digest(password):
     with 12 rounds of salt/work. It converts password to
     'utf-8' before encryption.
     """
-    utf_pass = password.encode('utf-8')
-    return bcrypt.hashpw(utf_pass, bcrypt.gensalt(rounds=12))
+    utf_password = password.encode('utf-8')
+    return bcrypt.hashpw(utf_password,
+                         bcrypt.gensalt(rounds=12)).decode('utf-8')
+
+
+def password_matches(password, password_digest):
+    """
+    Returns true if password genarates password digest
+    """
+
+    utf_password = password.encode('utf-8')
+    utf_password_digest = password_digest.encode('utf-8')
+
+    if bcrypt.checkpw(utf_password, utf_password_digest):
+        return True
+    else:
+        return False
