ac7ea9c8f116c5e010c9aef6f96a3655ee890f00
wley3337 <wley3337@gmail.com>
2021-09-22 14:32:47 -0400

    Add logout rout

---
+++ api/routes/logout.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, request
+from http import HTTPStatus
+from flasgger import swag_from
+from flask_jwt_extended import unset_jwt_cookies
+
+logout = Blueprint('logout', __name__)
+
+
+@logout.route('/v1/logout', methods=["GET"])
+@swag_from({
+    'response': {
+        HTTPStatus.OK.value: {
+            'description':
+            'Logs out a user by removing the JWT token in the cookies',
+        }
+    }
+})
+def logout_user():
+    """Logs Out a user of the application and removes the JWT HTTP only cookie"""
+    response = jsonify({
+        "message": "log out successful",
+    })
+    unset_jwt_cookies(response)
+    return response, 200
