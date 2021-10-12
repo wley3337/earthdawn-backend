b4dc4a8ee34660af303f90404d9f1a4135c979a0
Will Ley <wley3337@gmail.com>
2021-10-12 08:56:33 -0400

    Add a ChamelCase schema class

--- api/services/marshmallow.py
+++ api/services/marshmallow.py
@@ -1,3 +1,13 @@
 from flask_marshmallow import Marshmallow
 
 ma = Marshmallow()
+
+
+def camelcase(s):
+    parts = iter(s.split("_"))
+    return next(parts) + "".join(i.title() for i in parts)
+
+
+class CamelCaseSchema(ma.Schema):
+    def on_bind_field(self, field_name, field_obj):
+        field_obj.data_key = camelcase(field_obj.data_key or field_name)
